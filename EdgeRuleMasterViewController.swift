78bcf7d1fbe2fec38315b77eb4aba273516597ba
Grant <grantmac@grants-macbook-pro.local>
2016-06-13 10:34:02 -0500

    edge rule picker view added

---
+++ EdgeRuleMasterViewController.swift
@@ -0,0 +1,36 @@
+//
+//  EdgeRuleMasterViewController.swift
+//  Electronic Eye Manager
+//
+//  Created by Grant on 6/13/16.
+//  Copyright © 2016 Skullcup. All rights reserved.
+//
+
+import UIKit
+
+var BCSPEdgeRule:NSArray = NSArray()
+
+class EdgeRuleMasterViewController: UIViewController {
+    
+    @IBOutlet var BuyCallSellPut: [UILabel]!
+    
+    @IBAction func EditBCSP(sender: AnyObject) {
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print(String(xonData[0][0]))
+        print(String(xonData[0][1]))
+        print(String(xonData[0][2]))
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+
