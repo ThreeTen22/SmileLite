0f49e000c8623a760bc65c8a80869e449ba68b5b
Grant <grantmac@grants-macbook-pro.local>
2016-06-13 17:36:25 -0500

    added information to strikes table

--- Electronic Eye Manager/TableViewController.swift
+++ Electronic Eye Manager/TableViewController.swift
@@ -26,16 +26,21 @@ class TableViewController: UITableViewController {
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return xonData.count
     }
-    
+    // Date  Strk CBid CAsk  CTheo Cpos PBid PAsk PTheo, PPos, PutDelta (11 total)
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
         let cell = self.tableView.dequeueReusableCellWithIdentifier("strikeCell", forIndexPath: indexPath) as! MonthEyeCell
+        let monthFocus = "06/17/16"
         
-        cell.strikePrice?.text = String(xonData[indexPath.row][1])
-        //cell.delta
-        
+        if String(xonData[indexPath.row][0]) == monthFocus {
+            cell.strikePrice?.text = String(xonData[indexPath.row][1])
+            cell.delta?.text = removeAfterIndex(Source: String(xonData[indexPath.row][10]), CutOffIndex: 6)
+            cell.cTheo?.text = removeAfterIndex(Source: String(xonData[indexPath.row][3]), CutOffIndex: 4)
+            cell.cPos?.text = String(xonData[indexPath.row][5])
+            cell.pTheo?.text = removeAfterIndex(Source: String(xonData[indexPath.row][8]), CutOffIndex: 4)
+            cell.pPos?.text = String(xonData[indexPath.row][9])
+        }
         return cell
-        
     }
     
 }
