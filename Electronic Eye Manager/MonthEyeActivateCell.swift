5c107405ba65fbd25799e05d9a9d1d5c2dcf832d
Grant <grantmac@grants-macbook-pro.local>
2016-06-20 20:57:26 -0500

    Now shows the month the eye is focusing on.  With buttons which turn on and off for sc/bp/bc/sp

---
+++ Electronic Eye Manager/MonthEyeActivateCell.swift
@@ -0,0 +1,35 @@
+//
+//  MonthEyeActivateCell.swift
+//  Electronic Eye Manager
+//
+//  Created by Grant on 6/20/16.
+//  Copyright © 2016 Skullcup. All rights reserved.
+//
+
+import UIKit
+
+class MonthEyeActivateCell: UITableViewCell {
+    
+    
+    var isBtnActive = [false, false, false, false]
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        for i in 1...4 {
+            if let btn = self.viewWithTag(i) as! UIButton! {
+                btn.layer.borderWidth = 1.0
+                btn.setTitle("", forState: .Normal)
+            }
+            
+        }
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Electronic Eye Manager/MonthEyeMainViewController.swift
+++ Electronic Eye Manager/MonthEyeMainViewController.swift
@@ -8,8 +8,26 @@
 
 import UIKit
 
-class MonthEyeMainViewController: UIViewController {
+class MonthEyeMainViewController: UIViewController, UITableViewDelegate {
 
+    @IBOutlet var monthEyeActivateTable: UITableView!
+    
+    @IBAction func sellCallAtn(sender: UIButton) {
+        print(sender.superview)
+        changeActiveButton(0, sender)
+    }
+    @IBAction func buyPutAtn(sender: UIButton) {
+        changeActiveButton(1, sender)
+    }
+
+    @IBAction func buyCallAtn(sender: UIButton) {
+        changeActiveButton(2, sender)
+    }
+    
+    @IBAction func sellPutAtn(sender: UIButton) {
+        changeActiveButton(3, sender)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -19,6 +37,17 @@ class MonthEyeMainViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func changeActiveButton(index:Int,_ btnToModify:UIButton) {
+       let monthEyeActiveCell = monthEyeActivateTable.cellForRowAtIndexPath(NSIndexPath(forRow: 0, inSection: 0)) as! MonthEyeActivateCell
+        if (monthEyeActiveCell.isBtnActive[index]) {
+            btnToModify.backgroundColor = UIColor.whiteColor()
+            monthEyeActiveCell.isBtnActive[index] = false
+        } else {
+            btnToModify.backgroundColor = UIColor.blueColor()
+            monthEyeActiveCell.isBtnActive[index] = true
+        }
     }
     
 
@@ -31,5 +60,17 @@ class MonthEyeMainViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = monthEyeActivateTable.dequeueReusableCellWithIdentifier("MonthCell") as! MonthEyeActivateCell
+        if let monthName = cell.viewWithTag(5) as? UILabel {
+            monthName.text = "\(xonTitleData[0]) \(xonData[0][0])"
+        }
+        return cell
+    }
 
 }
