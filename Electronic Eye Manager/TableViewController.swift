e6f3069da07f432f02b180457b276bc777921a96
Grant <grantmac@grants-macbook-pro.local>
2016-06-20 21:14:31 -0500

    fixed a bug where there were too many tables, which led to duplicate strike entries.  This has been fixed.

--- Electronic Eye Manager/TableViewController.swift
+++ Electronic Eye Manager/TableViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class TableViewController: UITableViewController {
     var selectMinDelta: Bool = false
     var deltaRange = [-1,-1]
+    let monthFocus = "06/17/16"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,13 +24,19 @@ class TableViewController: UITableViewController {
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return xonData.count
+        var count = 0
+        for i in 0..<xonData.count {
+            if String(xonData[i][0]) == monthFocus {
+                count += 1
+            }
+        }
+        return count
     }
     // Date  Strk CBid CAsk  CTheo Cpos PBid PAsk PTheo, PPos, PutDelta (11 total)
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
         let cell = self.tableView.dequeueReusableCellWithIdentifier("strikeCell", forIndexPath: indexPath) as! MonthEyeCell
-        let monthFocus = "06/17/16"
+        
         
         if String(xonData[indexPath.row][0]) == monthFocus {
             cell.strikePrice?.text = String(xonData[indexPath.row][1])
